2653439980fdb956682a52040ecc064995b0657d
vaik10 <jaganivaidik@gmail.com>
2026-04-24 22:30:03 +0530

    feat: add datasource configuration using postgresql

--- expense-tracker/src/config/env.ts
+++ expense-tracker/src/config/env.ts
@@ -7,7 +7,10 @@ export const env = {
   port: Number(process.env.PORT ?? 3000),
 
   db: {
-    client: process.env.DB_CLIENT ?? 'sqlite3',
-    file: process.env.DB_FILE ?? './data/db.sqlite3',
+    host: process.env.DB_HOST ?? 'localhost',
+    port: Number(process.env.DB_PORT ?? 5432),
+    user: process.env.DB_USER ?? 'postgres',
+    password: process.env.DB_PASSWORD ?? 'postgres',
+    database: process.env.DB_NAME ?? 'expense_tracker',
   },
 };
